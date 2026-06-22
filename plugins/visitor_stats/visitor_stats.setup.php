52c3f49180ea90b78738a5c7a19dbe75eab9bbfc
webitproff <webitproff@gmail.com>
2026-06-23 00:46:14 +0300

    Add visitor_stats plugin - setup configuration

---
+++ plugins/visitor_stats/visitor_stats.setup.php
@@ -0,0 +1,32 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Code=visitor_stats
+Name=Visitor Statistics with Crawler Detection
+Category=stats
+Description=Advanced visitor statistics with crawler/bot detection using Crawler-Detect
+Version=1.0.0
+Date=2026-06-22
+Author=Development Team
+Copyright=(c) 2026 Your Company
+Notes=Requires hits plugin. Tracks visitors and filters bots.
+SQL=
+Auth_guests=R
+Lock_guests=W12345A
+Auth_members=R
+Lock_members=W12345A
+Requires_plugins=hits
+[END_COT_EXT]
+==================== */
+
+/**
+ * Visitor Statistics with Crawler Detection
+ * Tracks visitor activity and filters crawlers/bots
+ *
+ * @package VisitorStats
+ * @copyright (c) Cotonti Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Wrong URL');
+?>
